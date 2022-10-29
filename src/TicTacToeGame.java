297fdeeb86b3e72dc8374392f9f3eed883906fd8
Robert Ashfield <ashfielr@uni.coventry.ac.uk>
2022-10-29 14:56:39 +0100

    added grid setup

---
+++ src/TicTacToeGame.java
@@ -0,0 +1,15 @@
+
+public class TicTacToeGame {
+
+	private static String[][] grid;
+	
+	public static void main(String[] args) {
+
+		setupGrid();
+	}
+	
+	private static void setupGrid() {
+		grid = new String[3][3];
+	}
+
+}
