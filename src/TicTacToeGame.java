d12351a2c08d7d3b124c7a018478c73b7d748c24
Robert Ashfield <ashfielr@uni.coventry.ac.uk>
2022-10-29 20:04:13 +0100

    added showGrid method and whitespace for empty cells

--- src/TicTacToeGame.java
+++ src/TicTacToeGame.java
@@ -6,10 +6,27 @@ public class TicTacToeGame {
 	public static void main(String[] args) {
 
 		setupGrid();
+		showGrid();
 	}
 	
 	private static void setupGrid() {
 		grid = new String[3][3];
+		
+		// setting empty cells
+		for(int row=0; row<grid.length; row++) {
+			for(int col=0; col<grid[row].length; col++) {
+				grid[row][col] = " ";
+			}
+		}
+	}
+	
+	private static void showGrid() {
+		for(int row=0; row<grid.length; row++) {
+			for(int col=0; col<grid[row].length; col++) {
+				System.out.print(" | " + grid[row][col] + " |");
+			}
+			System.out.println();
+		}
 	}
 
 }
