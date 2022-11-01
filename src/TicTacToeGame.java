f9616ec254f6347352dc5e5d2dcc289f498ef38d
Robert Ashfield <ashfielr@uni.coventry.ac.uk>
2022-11-01 10:33:43 +0000

    added takeTurn method

--- src/TicTacToeGame.java
+++ src/TicTacToeGame.java
@@ -1,21 +1,28 @@
+import java.util.Scanner;
 
 public class TicTacToeGame {
 
-	private static String[][] grid;
+	private static char[][] grid;
+	private static Scanner scan = new Scanner(System.in);
 	
 	public static void main(String[] args) {
-
+		boolean  gameRunning = true;
 		setupGrid();
-		showGrid();
+		while(gameRunning) {
+			showGrid();
+			takeTurn('X');
+			showGrid();
+			takeTurn('O');
+		}
 	}
 	
 	private static void setupGrid() {
-		grid = new String[3][3];
+		grid = new char[3][3];
 		
 		// setting empty cells
 		for(int row=0; row<grid.length; row++) {
 			for(int col=0; col<grid[row].length; col++) {
-				grid[row][col] = " ";
+				grid[row][col] = ' ';
 			}
 		}
 	}
@@ -27,6 +34,32 @@ public class TicTacToeGame {
 			}
 			System.out.println();
 		}
+	}
+	
+	private static void takeTurn(char c) {
+		boolean cellNotSelected = true;
+		try {
+			while (cellNotSelected) {
+				System.out.println("Enter row (0,1, or 2): ");
+				byte row = scan.nextByte();
+				System.out.println(row);
+				System.out.println("Enter column (0,1, or 2): ");
+				byte col = scan.nextByte();
+				System.out.println(col);
+				System.out.println("(" + row + "," + col + ")");
+				
+				if (grid[row][col] == ' ') {
+					grid[row][col] = c;
+					cellNotSelected = false;
+				} else {
+					System.out.println("That cell is already used...");
+				}
+			}
+		}
+		catch (Exception e) {
+			System.out.println(e);
+		}
+
 	}
 
 }
